825cca6a639a231e2090d605bacdfb1ab78de196
Armando Rivera Negrete <manuel.rivera@kavak.com>
2021-10-20 11:26:45 -0500

    feat(onboarding): onboarding/components loader component generated

--- libs/frontend/onboarding/components/src/index.ts
+++ libs/frontend/onboarding/components/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/hero/hero.module';
 export * from './lib/circle-progress-bar/circle-progress-bar.module';
 export * from './lib/movie-credits/movie-credits.module';
 export * from './lib/movie-summary/movie-summary.module';
+export * from './lib/person-card/person-card.module';
+export * from './lib/loader/loader.module';
