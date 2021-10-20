7073541e009f803f7b12d21710297137c1a52c63
Armando Rivera Negrete <manuel.rivera@kavak.com>
2021-10-20 18:32:57 -0500

    feat(onboarding-components): movie-card done

--- apps/onboarding/src/app/app.module.ts
+++ apps/onboarding/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { MovieCardModule } from '@olimpo/frontend/onboarding/components';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule],
+  imports: [BrowserModule, RouterModule, MovieCardModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- libs/frontend/onboarding/components/src/lib/movie-card/movie-card.component.ts
+++ libs/frontend/onboarding/components/src/lib/movie-card/movie-card.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'frontend-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss'],
 })
-export class MovieCardComponent {}
+export class MovieCardComponent {
+
+  @Input() id = '';
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() overview = '';
+  @Input() hover = false;
+
+  constructor() {
+    console.log('it works');
+  }
+
+}
