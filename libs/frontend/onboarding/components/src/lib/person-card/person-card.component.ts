ddb2dc18632506b470dfefe5a2522483c70d6b53
Armando Rivera Negrete <manuel.rivera@kavak.com>
2021-10-20 11:23:08 -0500

    feat(onboarding): onboarding/components person-card component generated

---
+++ libs/frontend/onboarding/components/src/lib/person-card/person-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frontend-person-card',
+  templateUrl: './person-card.component.html',
+  styleUrls: ['./person-card.component.scss'],
+})
+export class PersonCardComponent {}
